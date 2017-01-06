f7b87423182bc9008bbb002516e03c28f6ff4bdf
Petr Bažout <bazout.p@gmail.com>
2017-01-06 14:52:10 +0100

    Update typescript to newer version and remove tsd

--- typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="es6-promise/es6-promise.d.ts" />
-/// <reference path="jasmine/jasmine.d.ts" />
-/// <reference path="nock/nock.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="underscore/underscore.d.ts" />
