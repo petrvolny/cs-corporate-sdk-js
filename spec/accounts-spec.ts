ab010231104aadcdae0d104fa44d15d626758f05
Petr Bažout <bazout.p@gmail.com>
2017-01-25 11:12:17 +0100

    Fix travis now maybe?

--- spec/accounts-spec.ts
+++ spec/accounts-spec.ts
@@ -46,12 +46,9 @@ const testTransactions = response => {
   expect(item.transactionTypeI18N).toBe('D.platba - S24/TB/GSM/B24');
   expect(item.variableSymbol).toBe('16273');
   expectDate(item, {
-    bookingDate: '2016-05-19',
-    valuationDate: '2016-05-19',
+    bookingDate: '2016-06-20T00:00:00+02:00',
+    valuationDate: '2016-06-20T00:00:00+02:00',
   });
-
-  // console.log(item.bookingDate, item.bookingDate.toString(), 'dates', CSCoreSDK.EntityUtils.parseISODate(new Date(2016, 5, 20)), new Date(CSCoreSDK.EntityUtils.parseISODate(new Date(2016, 5, 20))), new Date(CSCoreSDK.EntityUtils.parseISODate(new Date(2016, 5, 20))).toString());
-  // expect(item.bookingDate.toString()).toBe(new Date(CSCoreSDK.EntityUtils.parseISODate(new Date(2016, 5, 20))).toString());
 
   expect(item.accountParty.accountNumber).toBe('223149101');
   expect(item.accountParty.bankCode).toBe('0100');
